7cd797098c57747fb9f7623bf23d0fd4e86b2750
Elder <echumaceroj@ucvvirtual.edu.pe>
2022-02-03 11:49:32 -0500

    creando datos falsos para las tablas

---
+++ database/factories/Post/CategoryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories\Post;
+
+use App\Models\Post\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+class CategoryFactory extends Factory
+{
+    protected $model = Category::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->unique()->word(20);
+        return [
+            'name'=>$name,
+            'slug'=>Str::slug($name),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post\Category;
+use App\Models\Post\Tag;
 use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Storage::deleteDirectory('posts');//borrando carpeta posts
+        Storage::makeDirectory('posts');//creando carpeta posts
+
+        $this->call(UserSeeder::class);
+        Category::factory(4)->create();
+        Tag::factory(8)->create();
+        $this->call(PostSeeder::class);
     }
 }
