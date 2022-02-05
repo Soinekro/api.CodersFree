136d6c81fd20f6eff98d29aaa09fabd49c1fb298
Elder <echumaceroj@ucvvirtual.edu.pe>
2022-02-04 19:39:57 -0500

    controlador para logueo- metodo store

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Post\CategoryController;
 use App\Http\Controllers\Api\Post\PostController;
 use App\Http\Controllers\Api\User\RegisterController;
@@ -10,3 +11,4 @@ Route::post('user/register',[RegisterController::class,'store'])->name('api.v1.u
 
 Route::apiResource('post/categories',CategoryController::class)->names('api.v1.posts.categories');
 Route::apiResource('post/posts',PostController::class)->names('api.v1.posts.posts');
+Route::post('login',[LoginController::class,'store']);
