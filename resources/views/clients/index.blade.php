4063d62dcc6f898690e87ab9546006ab6db69f9f
Elder <echumaceroj@ucvvirtual.edu.pe>
2022-02-08 09:12:30 -0500

    mostrar Clientes-modal

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -73,13 +73,16 @@
                                 <td class="py-2">
                                     @{{ client.name }}
                                 </td>
+                                {{-- mostrar Cliente --}}
                                 <td class="flex divide-x divide-gray-600 py-2">
-
-                                    <a class="pr-2 hover:text-blue-500 font-semibold cursor-pointer"
+                                    <a class="pr-2 hover:text-green-500 font-semibold cursor-pointer"
+                                        v-on:click="show(client)">Ver</a>
+                                    {{-- editar Cliente --}}
+                                    <a class="px-2 hover:text-blue-500 font-semibold cursor-pointer"
                                         v-on:click="edit(client)">editar</a>
+                                    {{-- eliminar Cliente --}}
                                     <a class="pl-2 hover:text-red-500 font-semibold cursor-pointer"
                                         v-on:click="destroy(client)">Eliminar</a>
-
                                 </td>
                             </tr>
                         </tbody>
@@ -123,17 +126,43 @@
                 </div>
             </x-slot>
             <x-slot name="footer">
-                <button type="button"
-                v-on:click="update()"
-                v-bind:disabled="editForm.disabled"
+                <button type="button" v-on:click="update()" v-bind:disabled="editForm.disabled"
                     class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm disabled:opacity-20">Actualizar</button>
                 <button type="button"
-                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" v-on:click="editForm.open = false">Cancel</button>
+                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    v-on:click="editForm.open = false">Cancel</button>
+            </x-slot>
+        </x-dialog-modal>
+        {{-- modal Ver Cliente --}}
+        <x-dialog-modal modal="showClient.open">
+            <x-slot name="title">
+                Ver Credenciales
+            </x-slot>
+            <x-slot name="content">
+                <div class="space-y-2">
+                    <p>
+                        <span>CLIENTE:</span><br>
+                        <span><b>@{{ showClient.name }}</b></span>
+                    </p>
+                    <p>
+                        <span>CLIENT_ID:</span><br>
+                        <span><b>@{{ showClient.id }}</b></span>
+                    </p>
+                    <p>
+                        <span>CLIENT_SECRET:</span><br>
+                        <span><b>@{{ showClient.secret }}</b></span>
+                    </p>
+
+                </div>
+
+            </x-slot>
+            <x-slot name="footer">
+                <button type="button"
+                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    v-on:click="showClient.open = false">Cancel</button>
             </x-slot>
         </x-dialog-modal>
     </div>
-
-
 
     @push('js')
 
@@ -152,11 +181,17 @@
                     editForm: {
                         open: false,
                         errors: [],
-                        id:null,
+                        id: null,
                         disabled: false,
                         errors: [],
                         name: null,
                         redirect: null,
+                    },
+                    showClient: {
+                        open: false,
+                        name: null,
+                        id: null,
+                        secret: null,
                     }
                 },
                 mounted() {
@@ -175,11 +210,7 @@
                             .then(response => {
                                 this.createForm.name = null;
                                 this.createForm.redirect = null;
-                                Swal.fire(
-                                    'Buen Trabajo!',
-                                    'Cliente Creado!',
-                                    'success'
-                                );
+                                this.show(response.data);
                                 this.getClients();
                                 this.createForm.disabled = false;
                                 this.createForm.errors = [];
@@ -189,15 +220,21 @@
                                 this.createForm.errors = [];
                             });
                     },
+                    show(client) {
+                        this.showClient.open = true;
+                        this.showClient.id = client.id;
+                        this.showClient.secret = client.secret;
+                        this.showClient.name = client.name;
+                    },
                     destroy(client) {
                         Swal.fire({
-                            title: 'Are you sure?',
-                            text: "You won't be able to revert this!",
+                            title: 'Estas Seguro de Eliminar a ' + client.name + '?',
+                            text: "Esta accion no se Podra Revertir!",
                             icon: 'warning',
                             showCancelButton: true,
                             confirmButtonColor: '#3085d6',
                             cancelButtonColor: '#d33',
-                            confirmButtonText: 'Yes, delete it!'
+                            confirmButtonText: 'Si, Eliminar!'
                         }).then((result) => {
                             if (result.isConfirmed) {
                                 axios.delete('/oauth/clients/' + client.id)
@@ -206,8 +243,8 @@
                                     });
 
                                 Swal.fire(
-                                    'Deleted!',
-                                    'Your file has been deleted.',
+                                    'Eliminado!',
+                                    'El Cliente Fue Eliminado con Exito!!.',
                                     'success'
                                 )
                             }
@@ -220,11 +257,11 @@
                         this.editForm.redirect = client.redirect;
                         this.editForm.errors = [];
                     },
-                    update(){
+                    update() {
 
                         this.editForm.disabled = true;
 
-                        axios.put('/oauth/clients/'+this.editForm.id, this.editForm)
+                        axios.put('/oauth/clients/' + this.editForm.id, this.editForm)
                             .then(response => {
                                 this.editForm.open = false;
                                 this.editForm.name = null;
